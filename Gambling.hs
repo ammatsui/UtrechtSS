ea07bb5170bb19e59131a8974e2baa6897895744
ammatsui <54817160+ammatsui@users.noreply.github.com>
2019-09-12 18:50:35 +0300

    Update Gambling.hs

--- Gambling.hs
+++ Gambling.hs
@@ -1,3 +1,6 @@
+-- Toss a coin six times and count the number of heads, then roll a dice; 
+-- if the number of eyes on the dice is greater than or equal to the number of heads then win, else lose. 
+
 
 import Control.Applicative 
 import Control.Monad       
@@ -6,6 +9,8 @@ import Data.Monoid
 import Data.Ratio          
 import System.Random       
 
+
+-- Exercise 2: Give Random instances for Coin and Dice.
 
 data Coin = H | T deriving (Bounded, Eq, Enum, Ord, Show)
 
@@ -31,6 +36,8 @@ class Monad m => MonadGamble m where
   roll :: m Dice
 
 
+-- Exercise 3: Give a MonadGamble instance for the IO monad.
+
 instance MonadGamble IO where
   toss = randomIO 
   roll = randomIO
@@ -40,7 +47,8 @@ count :: (a -> Bool) -> [a] -> Int
 count p = length . filter p
 
 
--- the game itself
+-- Exercise 1: Write a function game :: MonadGamble m => m Outcome that implements the game above.
+
 game :: MonadGamble m => m OutCome
 game = do
     coins <- replicateM 6 toss
@@ -48,7 +56,10 @@ game = do
     if ((1 + (fromEnum dRes)) >= (count (== H) coins)) then return Win else return Lose
 
 
--- simulation of the game
+-- Exercise 4: Write a function that runs a game of chance (given as the first parameter, not necessarily this game) 
+-- n times (n > 0, the second parameter) and returns the fraction of games won.
+
+-- simulation of a game
 simulate :: IO OutCome -> Integer -> IO Rational
 simulate g n = do
   ress     <- replicateM (fromIntegral n) g
@@ -57,6 +68,10 @@ simulate g n = do
 
 data DecisionTree a = Result a | Decision [DecisionTree a]
 
+
+-- Exercise 5: Give a Monad instance for DecisionTree. 
+-- Hint: use the types of (>>=) and return for guidance: 
+-- it’s the most straightforward, type-correct definition that isn’t an infinite loop.
 
 instance Functor DecisionTree where
   fmap = liftM
@@ -73,6 +88,8 @@ instance Monad DecisionTree where
   (Decision treelist) >>= f = Decision (map (>>= f) treelist) 
 
 
+-- Exercise 6: Give a MonadGamble instance for DecisionTree.
+
 instance MonadGamble DecisionTree where
   toss = Decision [Result H, Result T]
   roll = Decision [Result D1, Result D2, Result D3, Result D4, Result D5, Result D6]
@@ -83,6 +100,7 @@ listTree :: DecisionTree a -> [DecisionTree a]
 listTree (Result a) = [Result a]
 listTree (Decision xs) = Prelude.concatMap listTree xs
 
+-- Exercise 7: Write a function that, given a decision tree, computes the probability of winning.
 
 probabilityOfWinning :: DecisionTree OutCome -> Rational
 probabilityOfWinning g =
